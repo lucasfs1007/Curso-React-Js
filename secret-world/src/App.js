bbe174090e8b923e9f7287294db43e3bdea6d966
lucasfs1007 <lucasfs1007@gmail.com>
2022-07-13 10:35:31 -0300

    add first screen

---
+++ secret-world/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import StartScreen from './Components/StartScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <StartScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ secret-world/src/data/words.js
@@ -0,0 +1,10 @@
+export const wordList = {
+    carro:["Gol","Fusca","Mercedes","Palio","HB20","Kwid","Kicks","Citroen","Ferrarri","Polo","Golf"],
+    banda:["Queen","Beatles","Nirvana","Coldplay","Skank","Titãs","Ramones","ACDC","Metalica","Motorhead","Megadeath","Sepultura"],
+    profissão:["Médico","Advogado","Engenheiro","Programador","Professor","Cozinheiro","Vendedor","Motorista","Enfermeiro","Policial"],
+    cor:["Vermelho","Azul","Roxo","Amarelo","Verde","Marrom","Preto","Branco","Cinza"],
+    corpo:["Cabeça","Orelha","Pé","Mão","Boca","Braço","Nariz","Joelho","Pescoço","Peito"],
+    comida:["Pão","Pizza","Arroz","Feijão","Batata","Hamburguer","Couve","Feijoada","Cenoura","Pipoca","Tomate","Carne","Leite","Ovo"],
+    computador:["Gabinete","Mouse","Monitor","Teclado"],
+    programação:["Linguagem","Java","JavaScript","Python","HTML","CSS","React","Flutter","Github"],
+};
